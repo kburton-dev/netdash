30b26993af40031af8d760334db5584e8b62e2fe
Kyle <kyle-burton@live.co.za>
2023-12-28 16:28:30 +0200

    Install Octane, setup production Dockerfile with queue, scheduler, logging

---
+++ docker/prod/custom-cont-init.d/start.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/with-contenv bash
+
+touch /app/storage/logs/cron.log
+touch /app/storage/logs/laravel.log
+
+echo "Clearing cache, migrating DB, etc..."
+
+if [ ! -z "$PUID" ] && [ ! -z "$PGID" ]; then
+    echo "Setting permissions..."
+    chown -R $PUID:$PGID /app/
+fi
+
+cd /app
+
+gosu abc php artisan cache:clear
+gosu abc php artisan migrate --force
+
+# cache laravel config?
+# php artisan config:cache
+# cache laravel routes?
+# php artisan route:cache
