27d2d4a1e00023584430618d63e87d54c3c7f8b8
Kyle <kyle-burton@live.co.za>
2023-12-29 15:53:25 +0200

    Fix doc block and tag syncing for Feeds\SaveWithTags action

--- app/Actions/Feeds/SaveWithTags.php
+++ app/Actions/Feeds/SaveWithTags.php
@@ -7,22 +7,23 @@ namespace App\Actions\Feeds;
 use App\Feeds\FeedType;
 use App\Jobs\FetchFeedItems;
 use App\Models\Feed;
+use Carbon\CarbonInterface;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
 
 class SaveWithTags
 {
     /**
-     * @param  array{title: string, type: FeedType, url: string, tagIds: list<int>}  $data
+     * @param  array{title?: string, type?: FeedType, url?: string, last_fetch?: CarbonInterface, tagIds?: list<int>}  $data
      */
     public function __invoke(Feed $feed, array $data): Feed
     {
         return DB::transaction(function () use ($feed, $data): Feed {
-            $saved = save_model($feed, $data);
+            $saved = save_model($feed, Arr::except($data, 'tagIds'));
 
-            $feed->tags()->sync(
-                Arr::pull($data, 'tagIds', [])
-            );
+            if (array_key_exists('tagIds', $data)) {
+                $feed->tags()->sync($data['tagIds']);
+            }
 
             if ($feed->wasRecentlyCreated || $feed->wasChanged('url')) {
                 FetchFeedItems::dispatch($feed)->afterCommit();
