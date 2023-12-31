eab58dee815b28d63f8e9879f5e7fbbf307bf958
Kyle <kyle-burton@live.co.za>
2023-12-31 11:07:37 +0200

    Update start script

--- docker/prod/custom-cont-init.d/start.sh
+++ docker/prod/custom-cont-init.d/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bash
 
-touch /app/storage/logs/cron.log
-touch /app/storage/logs/laravel.log
+touch /app/storage/logs/cron.log && chown abc:abc /app/storage/logs/cron.log
+touch /app/storage/logs/laravel.log && chown abc:abc /app/storage/logs/laravel.log
 
 echo "Clearing cache, migrating DB, etc..."
 
