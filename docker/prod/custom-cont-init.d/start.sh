57d4337ac29ffd25c7ac551c6d1a490f45427a54
Kyle <kyle-burton@live.co.za>
2024-05-04 13:15:05 +0200

    Update build and start script

--- docker/prod/custom-cont-init.d/start.sh
+++ docker/prod/custom-cont-init.d/start.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/with-contenv bash
 
-touch /app/storage/logs/cron.log && chown abc:abc /app/storage/logs/cron.log
-touch /app/storage/logs/laravel.log && chown abc:abc /app/storage/logs/laravel.log
-
 echo "Clearing cache, migrating DB, etc..."
 
 if [ ! -z "$PUID" ] && [ ! -z "$PGID" ]; then
@@ -13,9 +10,6 @@ fi
 cd /app
 
 gosu abc php artisan cache:clear
+gosu abc php artisan optimize
 gosu abc php artisan migrate --force
-
-# cache laravel config?
-# php artisan config:cache
-# cache laravel routes?
-# php artisan route:cache
+# gosu abc php artisan storage:link
